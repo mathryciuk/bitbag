74fdae4fa461c2b9ebfb936c134e996463dc1745
mathryciuk <mat.hryciuk@gmail.com>
2022-07-14 20:05:36 +0200

    Colorable interface

--- src/Entity/Product/Product.php
+++ src/Entity/Product/Product.php
@@ -3,6 +3,9 @@
 declare(strict_types=1);
 
 namespace App\Entity\Product;
+use App\Service\Colorable;
+use App\Service\Color;
+
 
 use Doctrine\ORM\Mapping as ORM;
 use Sylius\Component\Core\Model\Product as BaseProduct;
@@ -12,10 +15,20 @@ use Sylius\Component\Product\Model\ProductTranslationInterface;
  * @ORM\Entity
  * @ORM\Table(name="sylius_product")
  */
-class Product extends BaseProduct
+class Product extends BaseProduct implements Colorable
 {
+
+    private $color;
+
+    public function Colorize(){
+
+        $coloring = new Color();
+       
+        $this->color = $coloring->getRGBcolor(); 
+    }
     protected function createTranslation(): ProductTranslationInterface
     {
         return new ProductTranslation();
     }
+
 }
---
+++ src/Service/Color.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service;
+
+class Color{
+
+    public function getRGBColor(){
+        // the body
+        return;
+    }
+    public function getStringColor(){
+        // the body 
+        return ;
+    }
+    
+}
